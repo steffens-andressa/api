282c58d9cec6ef032be02204f6f9427c239cfe79
Andressa Steffens <steffensand@gmail.com>
2024-02-12 17:00:09 -0300

    Create endpoint to save product

---
+++ src/main/java/com/example/api/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.example.api.repositories;
+
+import com.example.api.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID>{
+
+}
